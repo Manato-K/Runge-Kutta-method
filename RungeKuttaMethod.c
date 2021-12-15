76ee93d2b80412b6fe59c8791f039fe701e0e86a
chrom-P <kkllmm104@gmail.com>
2021-12-15 10:23:32 +0900

    why eng?

---
+++ RungeKuttaMethod.c
@@ -0,0 +1,25 @@
+/* Runge Kutta method */
+#include <stdio.h>
+#include <math.h>
+float f1(float x,float y)
+{ return(x+y); }
+int main(void)
+{
+    int i;
+    float x[100],y[100],h=0.2,r,A,B,C,D;
+    x[0]=0.0;
+    y[0]=0.0;
+    i=0;
+    printf(" x , y (error)\n");
+    do {
+        x[i+1]=x[i]+h;
+        A=h*f1(x[i],y[i]);
+        B=h*f1(x[i]+h/2.0,y[i]+A/2.0);
+        C=h*f1(x[i]+h/2.0,y[i]+B/2.0);
+        D=h*f1(x[i]+h,y[i]+C);
+        y[i+1]=y[i]+(A+2.0*B+2.0*C+D)/6.0;
+        i++;
+        r=exp(x[i])-x[i]-1;
+        printf("%f, %f (%f)\n",x[i],y[i],r-y[i]);
+    } while(x[i]<1.0);
+}
